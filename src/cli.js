4760500a85b4347300b693ffb4aefc8d34852317
João <joaovictorpfr@gmail.com>
2023-04-20 16:34:46 -0300

    add list links directorys

--- src/cli.js
+++ src/cli.js
@@ -4,14 +4,23 @@ import fs from 'fs';
 
 const caminho = process.argv;
 
+function imprimeList(result){
+    console.log(chalk.yellow('lista-links: '), result)
+}
+
 async function processar(argumentos){
     const caminho = argumentos[2];
 
     if(fs.lstatSync(caminho).isFile()){
         const result = await pegaArquivo(argumentos[2]);
-        console.log(chalk.yellow("link-list"), result);
+        imprimeList(result);
     }else if(fs.lstatSync(caminho).isDirectory()){
         const arquivos = await fs.promises.readdir(caminho);
+        arquivos.forEach(async (nomeFile) => {
+            const list = await pegaArquivo(`${caminho}/${nomeFile}`);
+            console.log(chalk.blue(`${caminho}/${nomeFile}`));
+            imprimeList(list);
+        })
         console.log(arquivos);
     }
 }
