9153044aa7a1ec00fdae6ca2a53e8bb1fc8cca20
João <joaovictorpfr@gmail.com>
2023-04-20 16:25:16 -0300

    a directory read test function in archives

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,19 @@
 import { pegaArquivo } from "./index.js";
 import chalk from "chalk";
+import fs from 'fs';
 
 const caminho = process.argv;
 
-async function processar(caminho){
-    const result = await pegaArquivo(caminho[2]);
-    console.log(chalk.yellow('link-list'), result);
+async function processar(argumentos){
+    const caminho = argumentos[2];
+
+    if(fs.lstatSync(caminho).isFile()){
+        const result = await pegaArquivo(argumentos[2]);
+        console.log(chalk.yellow("link-list"), result);
+    }else if(fs.lstatSync(caminho).isDirectory()){
+        const arquivos = await fs.promises.readdir(caminho);
+        console.log(arquivos);
+    }
 }
 
 processar(caminho);
