fca8bd2f872d7562da9d1ac54fe6d318a1de9a60
João <joaovictorpfr@gmail.com>
2023-04-22 11:28:26 -0300

    statur url code obj error

--- src/httpValidity.js
+++ src/httpValidity.js
@@ -1,22 +1,33 @@
+import chalk from "chalk";
 import { listLinks } from "./links.js";
 
 async function checkStatus(arrUrls) {
   const arrStatus = await Promise.all(
     arrUrls.map(async (url) => {
-      const res = await fetch(url);
+      try{ const res = await fetch(url);
       return res.status;
+      }catch(error){
+        return errors(error);
+      }
     })
   );
   return arrStatus;
 }
 
-export async function valideLinks(arrLinks) {
-  const status = await checkStatus(arrLinks);
-  return status;
+function errors(error){
+  const errorConsole = 'error...';
+  if(error.cause.code === 'ENOTFOUND'){
+    const strError = 'not-found!';
+    return errorConsole, strError;
+  }else{
+    return errorConsole;
+  }
 }
 
-/*
-
-[Teste de retorno 400](https://httpstat.us/404).
-[gatinho salsicha](http://gatinhosalsicha.com.br/)
-*/
+export async function valideLinks(arrLinks) {
+  const status = await checkStatus(arrLinks);
+  return arrLinks.map((obj, index) => ({
+    obj,
+    status: status[index],
+  }));
+}
